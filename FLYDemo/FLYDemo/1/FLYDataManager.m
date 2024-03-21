98c07efd75878c04c1c1d71d8711ff11566da091
周飞 <809176334@qq.com>
2024-03-21 11:29:17 +0800

    FLYDataManager.m

---
+++ FLYDemo/FLYDemo/1/FLYDataManager.m
@@ -0,0 +1,147 @@
+//
+//  FLYDataManager.m
+//  FXActionCamer
+//
+//  Created by RXTMacPro3 on 2024/3/20.
+//  Copyright © 2024 RIXTON. All rights reserved.
+//
+
+#import "FLYDataManager.h"
+
+@implementation FLYDataManager
+
+
+
+#pragma mark - public methods
+
+// 添加一个新的模型对象
++ (void)addModel:(id)model
+{
+    // 获取指定类的所有模型对象数组
+    NSMutableArray *models = [self getAllModelsForClass:[model class]];
+    
+    // 将新的模型对象添加到数组中
+    [models addObject:model];
+    
+    // 保存更新后的模型对象数组到文件
+    [self saveModels:models forClass:[model class]];
+}
+
+// 删除一个模型对象
++ (void)removeModel:(id)model
+{
+    // 获取指定类的所有模型对象数组
+    NSMutableArray *models = [self getAllModelsForClass:[model class]];
+    
+    // 从数组中移除指定的模型对象
+    [models removeObject:model];
+    
+    // 保存更新后的模型对象数组到文件
+    [self saveModels:models forClass:[model class]];
+}
+
+// 修改一个模型对象
++ (void)updateModel:(id)model
+{
+    // 获取指定类的所有模型对象数组
+    NSMutableArray *models = [self getAllModelsForClass:[model class]];
+    
+    // 查找指定模型对象在数组中的索引位置
+    NSUInteger index = [models indexOfObject:model];
+    
+    // 如果找到了指定模型对象
+    if (index != NSNotFound) {
+        // 用新的模型对象替换数组中相应位置的旧模型对象
+        [models replaceObjectAtIndex:index withObject:model];
+        
+        // 保存更新后的模型对象数组到文件
+        [self saveModels:models forClass:[model class]];
+    }
+}
+
+
+// 获取指定类的所有模型对象数组
++ (NSMutableArray *)getAllModelsForClass:(Class)modelClass
+{
+    // 从文件中读取保存的模型对象数组的二进制数据
+    NSData *data = [NSData dataWithContentsOfFile:[self filePathForModelsForClass:modelClass]];
+    
+    // 如果数据存在
+    if (data)
+    {
+        // 尝试解档数据为模型对象数组
+        NSError *error = nil;
+        
+        NSMutableSet * set = [NSMutableSet setWithArray:@[NSArray.class,NSDictionary.class, NSString.class, UIFont.class, NSMutableArray.class, NSMutableDictionary.class, NSMutableString.class, UIColor.class, NSMutableData.class, NSData.class, NSNull.class, NSValue.class,NSDate.class]];
+        
+        // 一定要把模型的class包含进集合，不然解档不出来
+        [set addObject:modelClass.class];
+        
+        NSArray *modelArray = [NSKeyedUnarchiver unarchivedObjectOfClasses:set fromData:data error:&error];
+        
+        // 如果解档出错
+        if (error) 
+        {
+            // 输出错误信息
+            NSLog(@"Unarchive error: %@", error);
+            // 返回一个空的模型对象数组
+            return [NSMutableArray array];
+        }
+        
+        // 否则返回解档得到的模型对象数组
+        return [NSMutableArray arrayWithArray:modelArray];
+    }
+    
+    // 如果数据不存在，则返回一个空的模型对象数组
+    return [NSMutableArray array];
+}
+
+
+
+#pragma mark - private methods
+
+// 保存模型对象数组到文件
++ (void)saveModels:(NSMutableArray *)models forClass:(Class)modelClass
+{
+    // 将模型对象数组转换为二进制数据
+    NSError *error = nil;
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:models requiringSecureCoding:YES error:&error];
+    
+    // 如果转换出错
+    if (error) 
+    {
+        // 输出错误信息
+        NSLog(@"Archive error: %@", error);
+        // 返回
+        return;
+    }
+    
+    // 将二进制数据写入文件
+    [data writeToFile:[self filePathForModelsForClass:modelClass] atomically:YES];
+}
+
+// 获取保存模型对象的文件路径
++ (NSString *)filePathForModelsForClass:(Class)modelClass
+{
+    
+    // 保存模型数据的文件夹路径
+    NSString * directoryPath = [[FLYFileManager documentsPath] stringByAppendingPathComponent:@"ModelData"];
+    
+    // 如果文件夹不存在
+    if ( [FLYFileManager fileExistsWithPath:directoryPath] == NO )
+    {
+        // 创建一个用于保存模型数据的子文件夹名称
+        [FLYFileManager createDirectory:@"ModelData" path:[FLYFileManager documentsPath]];
+    }
+    
+    // 使用模型类的类名作为文件名
+    NSString *fileName = NSStringFromClass(modelClass);
+    NSString * modelPath = [directoryPath stringByAppendingPathComponent:fileName];
+    
+    // 返回模型文件路径
+    return modelPath;
+}
+
+
+
+@end
