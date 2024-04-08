2052cfa758e6e7b8697d2a12494c9bf0ba8db59f
周飞 <809176334@qq.com>
2024-04-08 14:43:55 +0800

    Update FLYDeviceModel.h

--- FLYDemo/FLYDemo/1/FLYDeviceModel.h
+++ FLYDemo/FLYDemo/1/FLYDeviceModel.h
@@ -15,13 +15,20 @@
 ***************************************************/
 
 #import <Foundation/Foundation.h>
+#import "FLYDataProtocol.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FLYDeviceModel : NSObject <NSSecureCoding>
+@interface FLYDeviceModel : NSObject <NSSecureCoding, FLYDataProtocol>
+
+// 遵守 FLYDataProtocol 协议，必须实现 identity 属性 (用来查找用的) (不用外界赋值，每次创建本类的时候，内部都会给它赋值)
+@property (nonatomic,readonly) NSString * identity;
 
 @property (nonatomic,strong) NSString * wifiName;
 @property (nonatomic,strong) NSString * wifiPassword;
+//型号
+@property (nonatomic,strong) NSString * modelNumber;
+
 
 @end
 
