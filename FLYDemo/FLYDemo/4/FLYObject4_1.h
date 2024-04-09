d5fa14047404afbb26908d5e69936e91be061fd2
周飞 <809176334@qq.com>
2024-04-09 15:25:19 +0800

    Update FLYReadDocumentData.h

--- FLYDemo/FLYDemo/4/FLYObject4_1.h
+++ FLYDemo/FLYDemo/4/FLYObject4_1.h
@@ -1,15 +1,25 @@
 //
-//  FLYObject4_1.h
-//  FLYDemo
+//  FLYReadDocumentData.h
+//  FLYKit
 //
-//  Created by fly on 2024/3/21.
+//  Created by fly on 2024/4/9.
+//  Copyright © 2024 fly. All rights reserved.
 //
+
+
+/*
+ 
+ 从系统的"文件"app中获取数据，包括插在充电口的sd卡里也能获取
+ 
+ */
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FLYObject4_1 : NSObject
+@interface FLYReadDocumentData : NSObject
+
++ (void)readDocumentData;
 
 @end
 
